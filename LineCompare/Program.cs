5e3d300400f23a57a57470324404f8dd40172b47
Arvind Gill <arvindkhtmuz@gmail.com>
2022-08-24 09:06:13 +0530

    [arvind] Add: branch2 created

--- LineCompare/Program.cs
+++ LineCompare/Program.cs
@@ -5,7 +5,19 @@
         static void Main(string[] args)
         {
             LineComparession lineCompProblem = new LineComparession(1, 2, 3, 4);
-            lineCompProblem.CalculateLength();
+            double line1 = lineCompProblem.CalculateLength();
+            Console.WriteLine("length of the first line" + line1);
+            LineComparession lineCompProblem2 = new LineComparession(1, 2, 3, 4);
+            double line2 = lineCompProblem2.CalculateLength();
+            Console.WriteLine("length of the first line" + line2);
+            if (line1 == line2)
+            {
+                Console.WriteLine("line1 is equal to line2");
+            }
+            else
+            {
+                Console.WriteLine("line1 is not equal to line2");
+            }
         }
     }
 }
