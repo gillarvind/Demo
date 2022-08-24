f8b97585fcf6221efb807c38cfb7ba638e20df4f
Arvind Gill <arvindkhtmuz@gmail.com>
2022-08-24 08:50:56 +0530

    [arvind] Add: folder structure created

---
+++ LineCompare/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LineCompare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
